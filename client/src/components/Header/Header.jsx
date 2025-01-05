65b4516ca40dbeef4a52726d934eebdff93accd3
S-reyapandey <shreyaamt02@gmail.com>
2025-01-05 17:27:01 +0530

    google translator

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -53,7 +53,7 @@ const nav_links = [
     id: "contact",
     path: "/contact",
     display: "Contact Us",
-  }
+  },
 ];
 
 const drawerWidth = 400;
@@ -69,7 +69,8 @@ function Header({ navLinks = nav_links }) {
 
   const drawer = (
     <Box
-      onClick={handleDrawerToggle} role="presentation"
+      onClick={handleDrawerToggle}
+      role="presentation"
       sx={{
         textAlign: "center",
         bgcolor: "#F8F7F0",
@@ -91,11 +92,7 @@ function Header({ navLinks = nav_links }) {
           alignItems: "center",
         }}
       >
-        <img
-          src="/logo.png"
-          alt="Logo"
-          style={{ width: "150px" }}
-        />
+        <img src="/logo.png" alt="Logo" style={{ width: "150px" }} />
       </Box>
 
       <List
@@ -111,74 +108,72 @@ function Header({ navLinks = nav_links }) {
             "&:hover": {
               color: "#1C3917",
               fontSize: "2.2rem",
-              transform: "scale(1.1)", 
-        backgroundColor: "transparent", 
+              transform: "scale(1.1)",
+              backgroundColor: "transparent",
             },
           },
         }}
       >
         {navLinks.map((item) => (
           <React.Fragment key={item.id}>
-          <ListItem disablePadding>
-          <ListItemButton
-            sx={{
-              textAlign: "left",
-              transition: "all 0.3s ease",
-            "&:hover": {
-              color: "#1C3917",
-              fontSize: "2.2rem",
-              transform: "scale(1.1)", 
-            },
-            }}
-            component={Link}
-            to={item.path}
-          >
-            <ListItemText primary={item.display} />
-          </ListItemButton>
-        </ListItem>
-        <Divider
-      variant="fullWidth"
-      sx={{
-        width: "100%",
-        borderColor: "rgba(85, 87, 81, 0.5)",
-        marginBottom: "0.8rem",
-        marginTop: "0.5em"
-      }}
-    />
+            <ListItem disablePadding>
+              <ListItemButton
+                sx={{
+                  textAlign: "left",
+                  transition: "all 0.3s ease",
+                  "&:hover": {
+                    color: "#1C3917",
+                    fontSize: "2.2rem",
+                    transform: "scale(1.1)",
+                  },
+                }}
+                component={Link}
+                to={item.path}
+              >
+                <ListItemText primary={item.display} />
+              </ListItemButton>
+            </ListItem>
+            <Divider
+              variant="fullWidth"
+              sx={{
+                width: "100%",
+                borderColor: "rgba(85, 87, 81, 0.5)",
+                marginBottom: "0.8rem",
+                marginTop: "0.5em",
+              }}
+            />
           </React.Fragment>
-          
         ))}
       </List>
       <Box
-      padding="2rem"
-      display="flex"
-      alignItems="center"
-      justifyContent="center"
-      gap={4}
-      sx={{
-        display: { xs: "flex", sm: "flex", md: "none", lg: "none" },
-        mr: 1,
-      }}
-    >
-      <div className={styles.circle1}>
-        <a
-          href="https://www.linkedin.com/company/amrit-organics-jaipur/"
-          target="_blank"
-        >
-          <IconButton sx={{ color: "#000" }} size="small">
-            <LinkedIn />
-          </IconButton>
-        </a>
-      </div>
-      <div className={styles.circle1}>
-        <a target="_blank">
-          <IconButton sx={{ color: "#000" }} size="small">
-            <Instagram />
-          </IconButton>
-        </a>
-      </div>
-    </Box>
-
+        padding="2rem"
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        gap={4}
+        sx={{
+          display: { xs: "flex", sm: "flex", md: "none", lg: "none" },
+          mr: 1,
+        }}
+      >
+        <div className={styles.circle1}>
+          <a
+            href="https://www.linkedin.com/company/amrit-organics-jaipur/"
+            target="_blank"
+          >
+            <IconButton sx={{ color: "#000" }} size="small">
+              <LinkedIn />
+            </IconButton>
+          </a>
+        </div>
+        <div className={styles.circle1}>
+          <a target="_blank">
+            <IconButton sx={{ color: "#000" }} size="small">
+              <Instagram />
+            </IconButton>
+          </a>
+        </div>
+      </Box>
     </Box>
   );
 
@@ -221,13 +216,13 @@ function Header({ navLinks = nav_links }) {
             <div className={styles.topLineItem}></div>
           </Box>
           {/*Header 1 */}
-          <AppBar className={styles.appbar1} style={{position: "static"}}>
-            <Container maxWidth="lg">
+          <AppBar className={styles.appbar1} style={{ position: "static" }}>
+            <Container maxWidth="xl">
               <Toolbar disableGutters>
                 {/*to wrap whole elements */}
                 <Box
                   display="flex"
-                  gap={8}
+                  gap={2}
                   flexWrap="wrap"
                   alignItems="center"
                   flexGrow={1}
@@ -245,20 +240,22 @@ function Header({ navLinks = nav_links }) {
                       alignItems: "center",
                     }}
                   >
-                    <img src="/logo.png" alt="Logo" style={{height: 63}} />
+                    <Link to="/">
+                      <img src="/logo.png" alt="Logo" style={{ height: 65 }} />
+                    </Link>
                   </Box>
                   {/* to wrap icons social media handles */}
                   <Box
                     display="flex"
                     justifyContent="flex-end"
                     alignItems="center"
-                    gap={3}
+                    gap={1}
                   >
                     <Box
                       display="flex"
                       alignItems="center"
                       justifyContent="center"
-                      gap={2}
+                      gap={1}
                       sx={{
                         display: { xs: "none", md: "none", lg: "flex" },
                         mr: 1,
@@ -270,21 +267,21 @@ function Header({ navLinks = nav_links }) {
                           target="_blank"
                         >
                           <IconButton sx={{ color: "#000" }} size="small">
-                            <LinkedIn />
+                            <LinkedIn style={{ fontSize: "1.2rem" }} />
                           </IconButton>
                         </a>
                       </div>
                       <div className={styles.circle}>
                         <a target="_blank">
                           <IconButton sx={{ color: "#000" }} size="small">
-                            <Instagram />
+                            <Instagram style={{ fontSize: "1.2rem" }} />
                           </IconButton>
                         </a>
                       </div>
                       <div className={styles.circle}>
-                        <a href="https://wa.me/9569501586 "target="_blank">
+                        <a href="https://wa.me/9569501586 " target="_blank">
                           <IconButton sx={{ color: "#000" }} size="small">
-                            <WhatsApp />
+                            <WhatsApp style={{ fontSize: "1.2rem" }} />
                           </IconButton>
                         </a>
                       </div>
@@ -294,7 +291,7 @@ function Header({ navLinks = nav_links }) {
                       display="flex"
                       justifyContent="flex-end"
                       alignItems="center"
-                      gap={2}
+                      gap={1}
                       sx={{
                         display: { xs: "none", md: "none", lg: "flex" },
                         mr: 1,
@@ -311,7 +308,7 @@ function Header({ navLinks = nav_links }) {
                           <a href="tel: +91 95695 01586" target="_blank">
                             {" "}
                             <IconButton
-                              sx={{ color: "#4BAF47", fontSize: "2rem" }}
+                              sx={{ color: "#4BAF47", fontSize: "2.1rem" }}
                               size="large"
                             >
                               <LuPhoneCall />
@@ -325,20 +322,25 @@ function Header({ navLinks = nav_links }) {
                             sx={{
                               color: "#878680",
                               fontWeight: "550",
-                              fontSize: "0.6rem",
-                              fontFamily: "Signika"
+                              fontSize: "0.75rem",
+                              fontFamily: "Signika",
                             }}
                           >
                             Call anytime
                           </Typography>
-                          <a href="tel: +91 95695 01586" target="_blank" className="no-underline" style={{textDecoration: 'none'}}>
+                          <a
+                            href="tel: +91 95695 01586"
+                            target="_blank"
+                            className="no-underline"
+                            style={{ textDecoration: "none" }}
+                          >
                             <Typography
                               variant="h6"
                               sx={{
                                 color: "#000",
                                 fontWeight: "bold",
-                                fontSize: "0.8rem",
-                                fontFamily: "Roboto"
+                                fontSize: "0.95rem",
+                                fontFamily: "Roboto",
                               }}
                             >
                               +91 9569501586
@@ -368,7 +370,7 @@ function Header({ navLinks = nav_links }) {
                             rel="noopener noreferrer"
                           >
                             <IconButton
-                              sx={{ color: "#4BAF47", fontSize: "2rem" }}
+                              sx={{ color: "#4BAF47", fontSize: "2.1rem" }}
                               size="large"
                             >
                               <RiMailSendFill />
@@ -382,23 +384,25 @@ function Header({ navLinks = nav_links }) {
                             sx={{
                               color: "#878680",
                               fontWeight: "550",
-                              fontSize: "0.6rem",
-                              fontFamily: "Signika"
+                              fontSize: "0.75rem",
+                              fontFamily: "Signika",
                             }}
                           >
                             Send Mail
                           </Typography>
                           <a
                             href="mailto:sales@amritorganics.in"
-                            target="_blank" className="no-underline" style={{textDecoration: 'none'}}
+                            target="_blank"
+                            className="no-underline"
+                            style={{ textDecoration: "none" }}
                           >
                             <Typography
                               variant="h6"
                               sx={{
                                 color: "#000",
                                 fontWeight: "bold",
-                                fontSize: "0.8rem",
-                                fontFamily: "Roboto"
+                                fontSize: "0.95rem",
+                                fontFamily: "Roboto",
                               }}
                             >
                               sales@amritorganics.in
@@ -422,15 +426,13 @@ function Header({ navLinks = nav_links }) {
                         gap={1}
                       >
                         <div className={styles.iconB}>
-                          {" "}
                           <a
                             href="https://maps.app.goo.gl/xAAT4BADbVacRKMd9"
                             target="_blank"
                             rel="noopener noreferrer"
                           >
-                            {" "}
                             <IconButton
-                              sx={{ color: "#4BAF47", fontSize: "2rem" }}
+                              sx={{ color: "#4BAF47", fontSize: "2.1rem" }}
                               size="large"
                             >
                               <GrMapLocation />
@@ -444,8 +446,8 @@ function Header({ navLinks = nav_links }) {
                             sx={{
                               color: "#878680",
                               fontWeight: "550",
-                              fontSize: "0.6rem",
-                              fontFamily: "Signika"
+                              fontSize: "0.75rem",
+                              fontFamily: "Signika",
                             }}
                           >
                             Amrit Organics
@@ -453,15 +455,17 @@ function Header({ navLinks = nav_links }) {
                           <a
                             href="https://maps.app.goo.gl/xAAT4BADbVacRKMd9"
                             target="_blank"
-                            rel="noopener noreferrer" className="no-underline" style={{textDecoration: 'none'}}
+                            rel="noopener noreferrer"
+                            className="no-underline"
+                            style={{ textDecoration: "none" }}
                           >
                             <Typography
                               variant="h6"
                               sx={{
                                 color: "#000",
                                 fontWeight: "bold",
-                                fontSize: "0.8rem",
-                                fontFamily: "Roboto"
+                                fontSize: "0.95rem",
+                                fontFamily: "Roboto",
                               }}
                             >
                               Jaisalmer, Rajasthan
@@ -471,7 +475,9 @@ function Header({ navLinks = nav_links }) {
                       </Box>
                     </Box>
                   </Box>
+                 
                 </Box>
+                
               </Toolbar>
             </Container>
           </AppBar>
@@ -482,9 +488,9 @@ function Header({ navLinks = nav_links }) {
             style={{
               position: "fixed",
               top: isSticky ? "0" : "auto",
-              transform: isSticky ? "translateY(0)" : "translateY(80px)",
+              transform: isSticky ? "translateY(0)" : "translateY(94px)",
               transformStyle: "flat",
-              transition: "all 0.3s ease",
+              transition: "all 0.5s ease",
               zIndex: 1000,
               transitionDuration: "0.1s",
             }}
@@ -508,7 +514,7 @@ function Header({ navLinks = nav_links }) {
                     aria-controls="menu-appbar"
                     aria-haspopup="true"
                     onClick={handleDrawerToggle}
-                    color="inherit" 
+                    color="inherit"
                   >
                     <Menu />
                   </IconButton>
@@ -516,27 +522,52 @@ function Header({ navLinks = nav_links }) {
                 <Box
                   display="flex"
                   justifyContent="center"
-                  gap={3}
+                  gap={4}
                   sx={{
                     display: { xs: "none", sm: "none", md: "flex", lg: "flex" },
-                    
+                    marginBottom: "-8px",
+                    "&: hover": {
+                      color: "transparent",
+                    },
                   }}
                 >
                   {navLinks.map((link) => (
-                    <Typography key={link.id} variant="body1" color="white">
+                    <Typography
+                      key={link.id}
+                      variant="body1"
+                      color="white"
+                      sx={{
+                        textAlign: "center",
+                        fontSize: "1.2rem",
+                      }}
+                    >
                       <Link
                         to={link.path}
-                        style={{ color: "inherit", textDecoration: "none" }}
-                        aria-label={link.display} className="no-underline"
-                        
+                        className={styles.navlink}
+                        style={{
+                          color: "inherit",
+                          textDecoration: "none",
+                          display: "inline-block",
+                          position: "relative",
+                          padding: "4px 8px",
+                          fontFamily: "Signika",
+                          fontSize: "inherit",
+                        }}
+                        aria-label={link.display}
                       >
-                       <ListItem disableGutters><ListItemButton sx={{ transition:"all 0.2s ease", fontFamily: "Signika","&:hover":{
-                         transform: "scale(1.1)", fontSize: "1.1rem", color: "#E1C16E"
-                       }
-                        
-                       }}>{link.display}
-                       </ListItemButton></ListItem>
-                        
+                        <ListItem disableGutters>
+                          <ListItemButton
+                            sx={{
+                              fontFamily: "Signika",
+                              "&:hover": {
+                                color: "#E1C16E",
+                              },
+                            }}
+                          >
+                            {link.display}
+                            <span className={styles.navlink2} />
+                          </ListItemButton>
+                        </ListItem>
                       </Link>
                     </Typography>
                   ))}
